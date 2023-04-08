1ce70a17f6cfff6c8fab581d3066315a11f23a42
Involts <100028421+Involts@users.noreply.github.com>
2023-04-08 18:54:27 +0800

    Staging for New Release

--- js/Main.js
+++ js/Main.js
@@ -1,6 +1,7 @@
 const nav = document.getElementById("nav")
 const navcheck = document.getElementById("check")
 const screenWidth = window.innerWidth;
+const headers= document.getElementsByTagName('h1', "h2", "h3", "h4", "h5")
 window.addEventListener("resize", function () {
     if (window.innerWidth !== screenWidth) {
         // Do something
@@ -18,4 +19,56 @@ navcheck.addEventListener('click', () => {
     nav.style.position = "static";
     document.documentElement.style.overflow = "auto";
     }
+});
+// window.onscroll = function() {NavBar()};
+
+// var navbar = document.getElementById("nav");
+// var navbaroffset = navbar.offsetTop;
+
+// function NavBar() {
+//   if (window.pageYOffset >= 30) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+// function scrollTo(element, to, duration) {
+//   if (duration <= 0) return;
+//   var difference = to - element.scrollTop;
+//   var perTick = difference / duration * 10;
+
+//   setTimeout(function() {
+//       element.scrollTop = element.scrollTop + perTick;
+//       if (element.scrollTop === to) return;
+//       scrollTo(element, to, duration - 10);
+//   }, 10);
+// }
+// scrollTo(document.body, headers.offsetTop, 600);
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    if (document.querySelector("#toc")) {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        if (entry.isIntersecting === true) {
+          document.querySelector(`main li a[href="#${id}"]`).parentElement.classList.add("active");
+          document
+            .querySelector(`main li a[href="#${id}"]`)
+            .parentElement.classList.remove("decative");
+        } else {
+          document
+            .querySelector(`main li a[href="#${id}"]`)
+            .parentElement.classList.remove("active");
+          document
+            .querySelector(`main li a[href="#${id}"]`)
+            .parentElement.classList.add("decative");
+        }
+      });
+    }
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("h1[id], h2[id], h3[id]").forEach((h1, h2, h3) => {
+    observer.observe(h1, h2, h3);
+  });
 });
